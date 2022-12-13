785a628a223cba3f843e032c1093a5d7aead197e
Camilo <camilocsm@gmail.com>
2022-12-13 18:12:16 -0300

    testint done

--- source/tools/MyPinTool/MyPinTool.cpp
+++ source/tools/MyPinTool/MyPinTool.cpp
@@ -1,172 +1,229 @@
 /*
- * Copyright (C) 2007-2021 Intel Corporation.
- * SPDX-License-Identifier: MIT
+ * Copyright 2002-2019 Intel Corporation.
+ * 
+ * This software is provided to you as Sample Source Code as defined in the accompanying
+ * End User License Agreement for the Intel(R) Software Development Products ("Agreement")
+ * section 1.L.
+ * 
+ * This software and the related documents are provided as is, with no express or implied
+ * warranties, other than those that are expressly stated in the License.
  */
 
-/*! @file
- *  This is an example of the PIN tool that demonstrates some basic PIN APIs 
- *  and could serve as the starting point for developing your first PIN tool
- */
-
-#include "pin.H"
+#include <cstdio>
+#include <cassert>
 #include <iostream>
 #include <fstream>
-using std::cerr;
-using std::endl;
-using std::string;
+#include <map>
+#include <list>
 
-/* ================================================================== */
-// Global variables
-/* ================================================================== */
+#include "pin.H"
+#include "instlib.H"
 
-UINT64 insCount    = 0; //number of dynamically executed instructions
-UINT64 bblCount    = 0; //number of dynamically executed basic blocks
-UINT64 threadCount = 0; //total number of threads, including main thread
+using namespace std;
 
-std::ostream* out = &cerr;
+INSTLIB::FILTER filter;
 
 /* ===================================================================== */
-// Command line switches
-/* ===================================================================== */
-KNOB< string > KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "", "specify file name for MyPinTool output");
-
-KNOB< BOOL > KnobCount(KNOB_MODE_WRITEONCE, "pintool", "count", "1",
-                       "count instructions, basic blocks and threads in the application");
-
-/* ===================================================================== */
-// Utilities
+/* Structs                                                               */
 /* ===================================================================== */
 
-/*!
- *  Print out help message.
- */
-INT32 Usage()
-{
-    cerr << "This tool prints out the number of dynamically executed " << endl
-         << "instructions, basic blocks and threads in the application." << endl
-         << endl;
+// Struct to represent Unique Instruction
+struct UniqueInstr {
+    ADDRINT addr;
+    USIZE size;
+    USIZE exec_count;
 
-    cerr << KNOB_BASE::StringKnobSummary() << endl;
+    UniqueInstr(ADDRINT addr = 0, USIZE size = 0)
+	: addr(addr), size(size), exec_count(0) {}
+};
 
+// Representation of a group of unique instructions
+struct InstrGroup {
+    USIZE exec_count;
+    std::list<UniqueInstr*> instrs;
+
+    InstrGroup(USIZE size) : exec_count(0) {}
+};
+
+/* ===================================================================== */
+/* Way Pin handles command line parameters                               */
+/* ===================================================================== */
+
+KNOB<string> KnobInputFile(
+    KNOB_MODE_WRITEONCE,
+    "pintool",
+    "i",
+    "default",
+    "specify input file name"
+);
+
+KNOB<string> KnobOutputFile(
+    KNOB_MODE_WRITEONCE,
+    "pintool",
+    "o",
+    "default",
+    "specify output file name"
+);
+
+// Instructions HashMap
+std::map<ADDRINT, UniqueInstr*> instructions;
+// Instruction Group as list
+std::list<InstrGroup*> groups;
+
+/* ===================================================================== */
+/*  Form of fetching the instruction or creating a new one                */
+/* ===================================================================== */
+
+UniqueInstr* fetch_instr(ADDRINT addr, USIZE size = 0) {
+    UniqueInstr* instr = instructions[addr];
+    if (instr == 0) {
+        instr = new UniqueInstr(addr, size);
+        instructions[addr] = instr;
+    } else {
+        assert(instr->size == size);
+    }
+
+    return instr;
+}
+
+/* ===================================================================== */
+/* Read Instructions of another filename                                 */
+/* ===================================================================== */
+
+void read_instrs(const char* filename) {
+    FILE* f;
+    char buffer[255];
+    static char sep[] = ":\r\n";
+
+    f = fopen(filename, "r");
+    if (f != 0) {
+        while (fgets(buffer, sizeof(buffer), f)) {
+            ADDRINT addr = strtoull(strtok(buffer+2, sep), 0, 16);
+            USIZE size = atoi(strtok(0, sep));
+
+            UniqueInstr* instr = fetch_instr(addr, size);
+            instr->exec_count += strtoull(strtok(0, sep), 0, 10);
+        }
+
+        fclose(f);
+    }
+}
+
+/* ===================================================================== */
+/* Increases exec count of Instrucution group                            */
+/* ===================================================================== */
+
+VOID counter(InstrGroup* group) {
+    ++group->exec_count;
+}
+
+/* ===================================================================== */
+/* Creates the Trace that fetches the Basic blocks                       */
+/* ===================================================================== */
+
+VOID Trace(TRACE trace, VOID* v) {
+    if (!filter.SelectTrace(trace)) return;
+    // Visit every basic block in the trace.
+    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl)) {
+        USIZE num_instrs = BBL_NumIns(bbl);
+        InstrGroup* group = new InstrGroup(num_instrs);
+        groups.push_back(group);
+
+        // for each instruction in a bbl setup a memory address for it in the hash
+        for (INS ins = BBL_InsHead(bbl); INS_Valid(ins); ins = INS_Next(ins)) {
+            ADDRINT addr = INS_Address(ins);
+            USIZE size = INS_Size(ins);
+
+            UniqueInstr* instr = fetch_instr(addr, size);
+            group->instrs.push_back(instr);
+        }
+
+        BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR) counter, IARG_PTR, group, IARG_END);
+    }
+}
+
+/* ===================================================================== */
+/* Counts groups into the individual instructions in the Hash.           */
+/* ===================================================================== */
+
+void flush_groups() {
+    for (std::list<InstrGroup*>::iterator it = groups.begin(), ed = groups.end();
+            it != ed; it++) {
+        InstrGroup* group = *it;
+
+        for (std::list<UniqueInstr*>::iterator it2 = group->instrs.begin(), ed2 = group->instrs.end();
+                it2 != ed2; it2++) {
+            UniqueInstr* instr = *it2;
+
+            instr->exec_count += group->exec_count;
+        }
+    }
+}
+
+/* ===================================================================== */
+/* Creates stream of instr for print on file or screen                   */
+/* ===================================================================== */
+
+void dump_instrs(FILE* fp) {
+    FILE* output = fp != 0 ? fp : stdout;
+    for (std::map<ADDRINT, UniqueInstr*>::iterator it = instructions.begin(), ed = instructions.end();
+            it != ed; it++) {
+        UniqueInstr* instr = it->second;
+        fprintf(output ,"0x%lx:%lu:%lu\n", instr->addr, instr->size, instr->exec_count);
+    }
+
+    if (fp != 0)
+        fclose(fp);
+}
+
+/* ===================================================================== */
+/* Ends Application , Prints Data                                        */
+/* This function is called when the application exits                    */
+/* ===================================================================== */
+
+VOID Fini(INT32 code, VOID *v) {
+    flush_groups();
+    dump_instrs((FILE*) v);
+}
+
+/* ===================================================================== */
+/* Print Help Message                                                    */
+/* ===================================================================== */
+
+INT32 Usage() {
+    PIN_ERROR("This Pintool counts every instruction executed\n" 
+              + KNOB_BASE::StringKnobSummary() + "\n");
     return -1;
 }
 
 /* ===================================================================== */
-// Analysis routines
+/* Main                                                                  */
 /* ===================================================================== */
 
-/*!
- * Increase counter of the executed basic blocks and instructions.
- * This function is called for every basic block when it is about to be executed.
- * @param[in]   numInstInBbl    number of instructions in the basic block
- * @note use atomic operations for multi-threaded applications
- */
-VOID CountBbl(UINT32 numInstInBbl)
-{
-    bblCount++;
-    insCount += numInstInBbl;
-}
-
-/* ===================================================================== */
-// Instrumentation callbacks
-/* ===================================================================== */
-
-/*!
- * Insert call to the CountBbl() analysis routine before every basic block 
- * of the trace.
- * This function is called every time a new trace is encountered.
- * @param[in]   trace    trace to be instrumented
- * @param[in]   v        value specified by the tool in the TRACE_AddInstrumentFunction
- *                       function call
- */
-VOID Trace(TRACE trace, VOID* v)
-{
-    // Visit every basic block in the trace
-    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
-    {
-        // Insert a call to CountBbl() before every basic bloc, passing the number of instructions
-        BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)CountBbl, IARG_UINT32, BBL_NumIns(bbl), IARG_END);
-    }
-}
-
-/*!
- * Increase counter of threads in the application.
- * This function is called for every thread created by the application when it is
- * about to start running (including the root thread).
- * @param[in]   threadIndex     ID assigned by PIN to the new thread
- * @param[in]   ctxt            initial register state for the new thread
- * @param[in]   flags           thread creation flags (OS specific)
- * @param[in]   v               value specified by the tool in the 
- *                              PIN_AddThreadStartFunction function call
- */
-VOID ThreadStart(THREADID threadIndex, CONTEXT* ctxt, INT32 flags, VOID* v) { threadCount++; }
-
-/*!
- * Print out analysis results.
- * This function is called when the application exits.
- * @param[in]   code            exit code of the application
- * @param[in]   v               value specified by the tool in the 
- *                              PIN_AddFiniFunction function call
- */
-VOID Fini(INT32 code, VOID* v)
-{
-    *out << "===============================================" << endl;
-    *out << "MyPinTool analysis results: " << endl;
-    *out << "Number of instructions: " << insCount << endl;
-    *out << "Number of basic blocks: " << bblCount << endl;
-    *out << "Number of threads: " << threadCount << endl;
-    *out << "===============================================" << endl;
-}
-
-/*!
- * The main procedure of the tool.
- * This function is called when the application image is loaded but not yet started.
- * @param[in]   argc            total number of elements in the argv array
- * @param[in]   argv            array of command line arguments, 
- *                              including pin -t <toolname> -- ...
- */
-int main(int argc, char* argv[])
-{
-    // Initialize PIN library. Print help message if -h(elp) is specified
-    // in the command line or the command line is invalid
+int main(int argc, char * argv[]) {
+    // Initialize pin
     if (PIN_Init(argc, argv))
-    {
-        return Usage();
+      return Usage();
+
+    if (KnobInputFile.Value() != "default") {
+       read_instrs(KnobInputFile.Value().c_str());
     }
 
-    string fileName = KnobOutputFile.Value();
-
-    if (!fileName.empty())
-    {
-        out = new std::ofstream(fileName.c_str());
+    FILE* fp = 0;
+    if (KnobOutputFile.Value() != "default") {
+        fp = fopen(KnobOutputFile.Value().c_str(), "w");
     }
 
-    if (KnobCount)
-    {
-        // Register function to be called to instrument traces
-        TRACE_AddInstrumentFunction(Trace, 0);
+    // Register Instruction to be called to instrument instructions
+    TRACE_AddInstrumentFunction(Trace, 0);
 
-        // Register function to be called for every thread before it starts running
-        PIN_AddThreadStartFunction(ThreadStart, 0);
+    filter.Activate();
 
-        // Register function to be called when the application exits
-        PIN_AddFiniFunction(Fini, 0);
-    }
+    // Register fini function.
+    PIN_AddFiniFunction(Fini, fp);
 
-    cerr << "===============================================" << endl;
-    cerr << "This application is instrumented by MyPinTool" << endl;
-    if (!KnobOutputFile.Value().empty())
-    {
-        cerr << "See file " << KnobOutputFile.Value() << " for analysis results" << endl;
-    }
-    cerr << "===============================================" << endl;
-
-    // Start the program, never returns
+    // Start the program, never returns.
     PIN_StartProgram();
 
     return 0;
 }
-
-/* ===================================================================== */
-/* eof */
-/* ===================================================================== */
